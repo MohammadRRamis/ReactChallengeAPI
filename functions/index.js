48a806d3dade0426cc6ee3cff38148126318c832
Your Name <you@example.com>
2023-06-14 16:36:11 +0300

    feat: added github webhook

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,12 @@
 const { onRequest } = require('firebase-functions/v2/https');
 const fetch = require('node-fetch');
+const { Configuration, OpenAIApi } = require('openai');
+const { Octokit } = require('@octokit/rest');
+
+const configuration = new Configuration({
+  apiKey: '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',
+});
+const openai = new OpenAIApi(configuration);
 
 exports.getUser = onRequest(async (request, response) => {
   const { token } = request.query;
@@ -56,4 +63,35 @@ exports.getStarredRepos = onRequest(async (request, response) => {
     console.error(error);
     response.status(500).json({ error: 'Internal server error' });
   }
+});
+
+exports.githubWebhook = onRequest(async (request, response) => {
+  const { body } = request;
+  const issueDetails = body.issue.body;
+  const issueNumber = body.issue.number;
+
+  const chat_completion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: issueDetails,
+  });
+
+  const resultText = chat_completion.data.choices[0].text;
+
+  const octokit = new Octokit({
+    auth: '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',
+  });
+
+  if (body.sender.login !== 'React-Challenge-Bot1') {
+    try {
+      await octokit.issues.createComment({
+        owner: 'MohammadRRamis',
+        repo: 'ReactChallenge',
+        issue_number: issueNumber,
+        body: resultText,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  response.status(200).json({ success: true });
 });
